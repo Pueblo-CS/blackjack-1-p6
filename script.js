4ba2ae925b919c5f458cd414e589f4673c90964a
Nathaniel E Adams <47441281+nadamspueblo@users.noreply.github.com>
2024-11-20 15:24:19 +0000

    Day 3

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let deck = [];
 const cardSuits = ["c", "s", "d", "h"];
 const cardFaces = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"];
+let playerHand = [];
+let dealerHand = [];
 
 function hidePopup() {
     let popup = document.getElementById("play-popup");
@@ -11,6 +13,10 @@ function deal() {
     hidePopup();
     fillDeck();
     shuffle();
+    playerHand.push(getCard());
+    addPlayerCard(playerHand[0]);
+    playerHand.push(getCard()); 
+    addPlayerCard(playerHand[1]);
 }
 
 function fillDeck() {
@@ -27,6 +33,21 @@ function fillDeck() {
 function shuffle() {
     for (let i = 0; i < deck.length; i++){
         let randNum = Math.floor(Math.random() * deck.length);
-        alert(randNum);
+        let temp = deck[i];
+        deck[i] = deck[randNum];
+        deck[randNum] = temp;
     }
+}
+
+function getCard() {
+    let lastCard = deck.pop();
+    return lastCard;
+}
+
+function addPlayerCard(card) {
+    let playerArea = document.getElementById("player-area");
+    let cardDiv = document.createElement("div");
+    cardDiv.setAttribute("class", "card");
+
+    playerArea.appendChild(cardDiv);
 }
